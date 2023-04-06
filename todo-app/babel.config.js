59cb907ef8458027c2b9cf7484237e5a28ce249a
Alexandre <alexandreriosme@gmail.com>
2023-04-06 05:51:23 -0300

    add NativeWind

--- todo-app/babel.config.js
+++ todo-app/babel.config.js
@@ -3,7 +3,8 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      ["babel-plugin-root-import"],
+      "babel-plugin-root-import",
+      "nativewind/babel",
       [
         "module-resolver",
         {
